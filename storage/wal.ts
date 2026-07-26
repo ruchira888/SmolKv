0f18926f611c62b373c942b14faa0d114ac87d89
ruchira888 <patilruchira611@gmail.com>
2026-07-26 22:49:53 +0530

    feat(storage): add WAL for durable operation logging

---
+++ storage/wal.ts
@@ -0,0 +1,39 @@
+import fs from fs
+
+export class wal{
+  private file="database.log"
+
+  append(
+  operation:string,
+  key:string,
+  value:string
+  ){
+    const data={
+      operation,
+      key,
+      value
+    }
+    fs.appendFileSync(
+this.file,
+JSON.stringify(data)+"\n"
+)
+  }
+
+
+read(){
+
+const logs =
+fs.readFileSync(
+this.file,
+"utf-8"//decode raw bytes to readable txt
+)
+
+
+return logs
+.split("\n")
+.filter(Boolean)
+.map((line:string)=>JSON.parse(line))
+
+
+}
+}
