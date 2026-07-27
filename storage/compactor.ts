15fa2becf67b9172ce1d03840f71c757bc4ca66e
ruchira888 <patilruchira611@gmail.com>
2026-07-27 18:27:23 +0530

    feat(storage): add Compactor to merge SSTables and drop tombstones

--- storage/compactor.ts
+++ storage/compactor.ts
@@ -3,9 +3,45 @@ export class Compact{
    compact(){
         const files = fs.readdirSync("./data")
             .filter(file => file.endsWith(".sst"));
+            //end up holdin da correct value 4 every key
             const merged=new Map<string,string>();
-        console.log(
-          "SSTables found:",
-           files);
+            for(const file of files){
+            const content =
+fs.readFileSync(
+"./data/"+file,
+"utf-8"
+);
+
+const entries =
+JSON.parse(content);
+
+
+
+for(const [key,value] of entries){
+
+
+if(value==="__DELETE__"){
+
+merged.delete(key);
+
+}
+
+else{
+
+merged.set(
+key,
+value
+);
+
+}
+            }
+        
     }
+    fs.writeFileSync(
+"./data/merged.sst",
+JSON.stringify(
+Array.from(merged.entries())
+)
+);
+   }
 }
